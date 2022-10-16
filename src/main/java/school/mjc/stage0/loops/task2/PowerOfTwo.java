c9b9b0e846cce9ef257340778e1d89a3684d84cc
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-10-16 11:39:06 +0500

    1
    ###

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int i=0,p=1;
+        while (power>i){
+            p *= 2;
+            System.out.println("2^"+power+"="+p);
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i=0, k=0;
+        while (i<printToInclusive){
+            if (k==3) break;
+            else if (printToInclusive % (i+1)==0){
+                System.out.println(i);
+            }
+            i++;
+            k++;
+        }
     }
 }
